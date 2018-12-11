549163c1f00be8e0ad5b2336517aa4296704a1a4
CimpeanAndrei <44667251+CimpeanAndrei@users.noreply.github.com>
2018-12-11 18:32:51 +0200

    Add files via upload

---
+++ Stacks.odd/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+class Stack
+{
+    public :
+        Stack(int i=0);
+        ~Stack();
+        void push(int x);
+        void displays();
+        int pop();
+        void displayodd();
+    private:
+        int lenght;
+        int top;
+        int *point;
+};
+
+Stack::Stack(int Size)
+{
+    top=-1;
+    lenght=Size;
+    if(Size==0)
+        point=0;
+    else
+        point=new int[lenght];
+}
+
+Stack::~Stack()
+{
+    if(point!=0)
+        delete [] point;
+}
+
+void Stack::push(int elem)
+{
+    if(point==0)
+    {
+        cout<<"Give the stack a size";
+        cin>>lenght;
+        point=new int[lenght];
+    }
+    if(top==(lenght-1))
+    {
+        cout<<"Cannot push into stack";
+        return;
+    }
+    else
+    {
+        top++;
+        point[top]=elem;
+    }
+
+}
+
+void Stack::displays()
+{
+    for(int i=top; i>0; i--)
+        cout<<" "<<point[i];
+    cout<<endl;
+}
+
+int Stack::pop()
+{
+    if(point==0 || top==-1)
+    {
+        cout<<"Stack is empty";
+        return -1;
+    }
+    int ret=point[top];
+    top--;
+    return ret;
+}
+
+void Stack::displayodd()
+{
+    for(int i=top; i>0; i--)
+        if(point[i]%2!=0)
+            cout<<" "<<point[i];
+
+    cout<<endl;
+}
+
+int main()
+{
+    Stack stack;
+    for(int i=0; i<10; i++)
+        stack.push(rand() %100);
+    stack.displays();
+    cout<<"How many times do you want to pop the stack: ";
+    int num;
+    cin>>num;
+    for(int j=0; j<num; j++)
+        stack.pop();
+    stack.displays();
+    cout<<"The odd numbers are: ";
+    stack.displayodd();
+    return 0;
+}
