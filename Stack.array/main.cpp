718d5320a3d351bc67c928fc8bfb99105e1eb427
CimpeanAndrei <44667251+CimpeanAndrei@users.noreply.github.com>
2018-12-11 18:34:22 +0200

    Add files via upload

---
+++ Stack.array/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#define STACK_SIZE 10
+#define EMPTY (-1)
+
+using namespace std;
+char elements[STACK_SIZE];
+int top=EMPTY;
+
+void push(char ch)
+{
+    elements[++top]=ch;
+}
+
+char pop()
+{
+    return elements[top--];
+}
+
+int StackEmpty()
+{
+    return top==EMPTY;
+}
+
+int StackFull()
+{
+	return top==STACK_SIZE-1;
+}
+
+void palindrome()
+{
+
+}
+
+int main()
+{
+    string palindrome;
+	int options;
+	char character;
+	int i, j;
+	bool ok;
+	do {
+		cout<<endl;
+		cout<<"	1. Add a  to the stack.\n";
+		cout<<"	2. Remove a  from the stack.\n";
+		cout<<"	3. Display stack contents\n";
+		cout<<"	4. Check if the stack is a palindrome\n";
+		cout<<"        5. Exit\n";
+		cout<<"	Please enter your choice-> ";
+        cin>>options;
+		switch ( options)
+        {
+			case 1:
+					if ( !StackFull() )
+                    {
+						cout<<(" Enter a single : ");
+						cin>>character;
+						push( character);
+					}
+					else
+						cout<<("Sorry, stack is full.")<<endl;
+					break;
+			case 2:
+					if ( !StackEmpty())
+						cout<<"\n Retrieved "<< pop();
+					else
+						cout<<" Sorry, stack is empty."<<endl;
+					break;
+			case 3: cout<<"\nThe stack contents is:\n";
+					for (i=top; i>=0; i--)
+						cout<<" "<< elements[ i];
+					break;
+			case 4:
+                   for(j=0; j<=top; j++)
+                        if(elements[j]==elements[top-j])
+                            ok=true;
+                        else
+                            ok=false;
+                    if(ok==true)
+                        cout<<"The stack is a palindrome" ;
+                    else
+                        cout<<"The stack is not a palindrome ";
+                    break;
+            case 5:
+                    cout<<"Exiting.. ";
+                    return 0;
+			default:
+					cout<<" Invalid option, please retry."<<endl;
+					break;
+		}
+
+	} while ( options != 4);
+	return 0;
+}
